3d187b62f2d672cb135fa363fe594526088187c9
Efe Topalak <efetopalak@Efes-Mac-mini.local>
2025-11-04 07:46:21 +0300

    end of the day

--- src/elite_craft/services/crawling.py
+++ src/elite_craft/services/crawling.py
@@ -1,1 +1,18 @@
-"""we will use crawl4ai to crawl the web site."""
+import asyncio
+from crawl4ai import AsyncWebCrawler#, BrowserConfig
+
+
+
+
+async def crawl(url:str):
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(url=url,)
+        print(result.markdown)
+
+
+def main():
+    asyncio.run(crawl(url="https://www.google.com/search?q=python"))
+if __name__ == "__main__":
+    main()
+
+
