c42555ff009db8b2d2c664cc1da9fcdace98b896
Efe Topalak <efetopalak@Efes-Mac-mini.local>
2025-11-04 03:24:54 +0300

    I designed the structure of folders

---
+++ src/elite_craft/services/embedding.py
@@ -0,0 +1,15 @@
+#add metadata into database, while searching through chunks if we filter the source that will save a lot of time.
+
+
+#Example of qdrant usage
+
+"""
+from qdrant_client import QdrantClient
+
+qdrant_client = QdrantClient(
+    url="https://05a4c951-02f7-4e8b-b746-98f77fa6ffce.eu-central-1-0.aws.cloud.qdrant.io:6333",
+    api_key="e.........6VVw",
+)
+
+print(qdrant_client.get_collections())
+"""
