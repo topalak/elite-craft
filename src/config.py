6befdfee4b221b0f3fb5444dc1fd9a2e87e0708c
Efe Topalak <efetopalak@Efes-Mac-mini.local>
2025-11-14 13:02:47 +0300

    nothing important

--- src/config.py
+++ src/config.py
@@ -2,12 +2,11 @@ import os
 from pydantic_settings import BaseSettings
 import datetime
 
-FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-ENV_FILE_DIR = os.path.abspath(os.path.join(FILE_DIR, os.pardir))
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))  #__file__ = current file
+ENV_FILE_DIR = os.path.abspath(os.path.join(FILE_DIR, os.pardir))  #os.pardir = os parent directory, that means python will look for .env file in parent folder
 
 class Settings(BaseSettings):
-    OLLAMA_HOST_TUNNEL: str = ""
-    OLLAMA_HOST_MY_LOCAL: str = ""
+    OLLAMA_HOST_LOCAL:str = ""
     OLLAMA_API_KEY: str = ""
     GROQ_API_KEY: str = ""
     SUPABASE_URL: str = ""
@@ -15,6 +14,7 @@ class Settings(BaseSettings):
     LANGSMITH_API_KEY: str = ""
     LANGSMITH_TRACING: str = "false"
     TAVILY_API_KEY: str = ""
+    QDRANT_API_KEY: str = ""
 
     OUTPUT: str = os.path.join(ENV_FILE_DIR, 'out')
     TIME_ZONE: datetime.timezone = datetime.timezone(offset=datetime.timedelta(hours=3), name='UTC+3')
